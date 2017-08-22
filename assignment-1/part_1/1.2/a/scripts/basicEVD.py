6056c86a6cb58f1b31503a6aabe3936b5a6047ba
alinalbook <alinalbook@gmail.com>
2017-08-22 14:01:13 +0530

    Basic working script for 1.1 (a)

--- assignment-1/part_1/1.2/a/scripts/basicEVD.py
+++ assignment-1/part_1/1.2/a/scripts/basicEVD.py
@@ -3,6 +3,7 @@
 import sys
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 # EigenValueDecomposition
 # This will decompose the input matrix A as: 
@@ -50,21 +51,40 @@ def writeImages(approxA, errorA, path):
     image[ : , col + pad : 2 * col + pad] = errorA 
     cv2.imwrite(path, image)
 
+def plotEigval(eig, eigenplot):
+    eig_index = range(len(eig) + 1)[1 : ]
+    abs_eig = []
+    for i in range(len(eig)):
+        abs_eig.append(np.absolute(eig[i]))
+
+    fig = plt.figure()
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_yscale('log')
+    ax.plot(eig_index, abs_eig)
+    plt.xlim([0, 260])
+
+    plt.xlabel('Eigen-index')
+    plt.ylabel('Eigenvalue (log)')
+    plt.title('Eigenvalue (log) .vs. Eigen-index')
+    plt.savefig(eigenplot)
+
 if __name__ == '__main__':
 
     if len(sys.argv) < 3:
-        print 'usage : python scripts/eigen.py <input-image> <rank> <output-image>'
+        print 'usage : python scripts/eigen.py <input-image> <rank> <output-image> <eigenplot>'
         print '<input-image> : path to input image'
         print '<rank> : order of approximation'
         print '<output-image> : path to output image'
         exit()
-    _, input_image, rank, output_image = sys.argv
+    _, input_image, rank, output_image, eigenplot = sys.argv
 
-    # Read image as matrix A
-    A = cv2.imread(input_image, 0)
+    # Read image as matrix A; normalize it
+    A = cv2.imread(input_image, 0) / 255.0
     print 'The image is of shape', A.shape
     # Do Eigenvalue Decomposition of A
     Q, eig, invQ = doEVD(A)
+    # Plot the eigenvalues of A
+    plotEigval(eig, eigenplot)
     # Order of approximation
     rank = np.int(rank)
     # Construct the approximate matrix with given rank
@@ -74,6 +94,6 @@ if __name__ == '__main__':
     # Compute the relative error in the approximation
     error = norm(errorA) / norm(A)
     print 'Relative error = %f' % error
-    # Write the reconstructed images to disk
-    writeImages(approxA, errorA, output_image)
+    # Write the reconstructed images to disk; re-scale images before writing
+    writeImages(approxA * 255.0, errorA * 255.0, output_image)
 
