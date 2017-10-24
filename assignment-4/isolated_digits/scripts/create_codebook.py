05e04109adf9d82f0377aa24b726656e3424d155
Karthik Thiagarajan <karthik@hyperverge.co>
2017-10-24 22:44:29 +0530

    Working code

--- assignment-4/isolated_digits/scripts/create_codebook.py
+++ assignment-4/isolated_digits/scripts/create_codebook.py
@@ -1,16 +1,21 @@
 import numpy as np
+import sys
 from sklearn.cluster import KMeans
 
-def cluster(data, K):
-    kmeans = KMeans(n_clusters = K, random_state = 0).fit(data)
-    return kmeans.cluster_centers_, kmeans.labels_
+def createCodeBook(K):
+    def cluster(data, K):
+        kmeans = KMeans(n_clusters = K, random_state = 0).fit(data)
+        return kmeans.cluster_centers_, kmeans.labels_
 
-all_frames = []
-for label in ['1', '5', 'z']:
-    feats = np.load('data/proc/train/%s.npy' % label)
-    all_frames.append(np.concatenate(feats))
-all_frames = np.concatenate(all_frames)
+    all_frames = []
+    for label in ['1', '5', 'z']:
+        feats = np.load('data/proc/train/%s.npy' % label)
+        all_frames.append(np.concatenate(feats))
+    all_frames = np.concatenate(all_frames)
 
-codes, code_asign = cluster(all_frames, 10)
+    codes, code_asign = cluster(all_frames, K)
 
-np.save('data/proc/codes.npy', codes)
+    np.save('data/proc/codes.npy', codes)
+    
+if __name__ == '__main__':
+    createCodeBook(int(sys.argv[1]))
--- assignment-4/isolated_digits/scripts/train.py
+++ assignment-4/isolated_digits/scripts/train.py
@@ -1,7 +1,8 @@
 import os
+import sys
 
-states = 5
-symbols = 10
+states = int(sys.argv[2])
+symbols = int(sys.argv[1])
 error = 0.01
 for label in ['1', '5', 'z']:
     os.system('./scripts/hmm/train_hmm data/proc/train/%s.seq 0 %s %s %s' % (str(label), str(states), str(symbols), str(error)))
